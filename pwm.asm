d4afb83dc9fbde578f2ecb78dbe2d2fb4f7967a7
BritishMenofLetters <73581730+BritishMenofLetters@users.noreply.github.com>
2022-06-17 11:16:34 +0200

    Add files via upload

---
+++ pwm.asm
@@ -0,0 +1,51 @@
+	push r16		
+	in r16, SREG
+	push r16
+
+//Ako je BROJAC_250 nula, zavrsi
+	cpi BROJAC_250, 0x00
+	BREQ KRAJ_PROGRAMA
+	
+//Ako je TEMP0 | TEMP1 | TEMP2 = 0
+//	*	Povecaj pwm za 20mV
+//	*	smanji  BROJAC_250
+//	*	postavi na binarni temp brojac na max (originalni binarni brojac R10 R9 R8)
+//
+// TEMP2 = R10
+// TEMP1 = R9
+// TEMP0 = R8
+//
+//
+	ldi r16, 0x00
+	or r16, TEMP0
+	or r16, TEMP1
+	or r16, TEMP2
+
+	BRNE SMANJI_TEMP	//Ako nije nula, smanji temp, inace smanji brojac_250	
+
+	//OCR1x + 262	
+
+	dec BROJAC_250
+
+	mov TEMP0, r8 
+	mov TEMP1, r9
+	mov TEMP2, r10
+	
+	jmp KRAJ_PWM
+
+//Inace smanji binarni temp brojac
+SMANJI_TEMP:
+	subi TEMP0, 0x01
+	sbci TEMP1, 0x00
+ 	sbci TEMP2, 0x00
+
+
+KRAJ_PWM:
+	pop r16
+	out SREG, r16
+	pop r16
+reti
+
+
+KRAJ_PROGRAMA:
+	jmp 0x0000
