2d4b21315fd5c112ff06bf3f38bc30f7d53f3d44
BritishMenofLetters <73581730+BritishMenofLetters@users.noreply.github.com>
2022-06-19 11:41:06 +0200

    Delete AvrBuild.bat

--- AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Users\student\Downloads\URS-main\URS-main\labels.tmp" -fI -W+ie -o "C:\Users\student\Downloads\URS-main\URS-main\URS_V1_1.hex" -d "C:\Users\student\Downloads\URS-main\URS-main\URS_V1_1.obj" -e "C:\Users\student\Downloads\URS-main\URS-main\URS_V1_1.eep" -m "C:\Users\student\Downloads\URS-main\URS-main\URS_V1_1.map" "C:\Users\student\Downloads\URS-main\URS-main\URS_V1_1.asm"
